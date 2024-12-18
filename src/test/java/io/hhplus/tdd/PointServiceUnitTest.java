a6eb1c09d3ec1ddc24c77c7f2c1c784aa1ebf559
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2024-12-19 00:29:02 +0900

    add: PointServiceTest - 테스트 클래스 구조 확립

--- src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
@@ -1,4 +1,44 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.point.PointService;
+import org.junit.jupiter.api.Nested;
+
+/**
+ * <b>{@link PointService} 단위 테스트 </b>
+ */
 public class PointServiceUnitTest {
+
+    /**
+     * <b>1. 포인트 충전 기능</b>
+     */
+    @Nested
+    class PointChargeTests {
+
+
+
+    }
+
+    /**
+     * <b>2. 포인트 사용 기능</b>
+     */
+    @Nested
+    class PointUsageTests {
+
+    }
+
+    /**
+     * <b>3. 포인트 잔액 조회 기능</b>
+     */
+    @Nested
+    class PointCheckTests {
+
+    }
+
+    /**
+     * <b>4. 포인트 충전 및 사용 내역 조회 기능</b>
+     */
+    @Nested
+    class PointHistoryTests {
+
+    }
 }
