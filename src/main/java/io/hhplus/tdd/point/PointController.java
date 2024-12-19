d3fa27220e6e5d466509fe19770cc513779dffcd
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2024-12-20 06:05:13 +0900

    DONE: TDD 기반 테스트 작성 및 구현

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
